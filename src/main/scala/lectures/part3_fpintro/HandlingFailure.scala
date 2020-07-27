90c0e617628d67f8d886ce2c3ed4bc7264a86e4a
Sergio Cano <serdeliverance@gmail.com>
2020-07-27 20:57:04 -0300

    handling failure

---
+++ src/main/scala/lectures/part3_fpintro/HandlingFailure.scala
@@ -0,0 +1,87 @@
+package lectures.part3_fpintro
+
+import scala.util.{Failure, Random, Success, Try}
+
+object HandlingFailure extends App {
+
+  // create success and failure
+  val aSuccess = Success(3)
+  val aFailure = Failure(new RuntimeException("SUPER FAILURE"))
+
+  println(aSuccess)
+  println(aFailure)
+
+  def unsafeMethod(): String = throw new RuntimeException("NO STRING FOR YOU")
+
+  // Try objects via the apply method
+  val potentialFailure = Try(unsafeMethod())
+  println(potentialFailure)
+
+  // syntax sugar
+  val anotherPotentialFailure = Try {
+    // code that might throw
+  }
+
+  // utilities
+  println(potentialFailure.isSuccess)
+
+  // orElse
+  def backupMethod(): String = "A valid result"
+  val fallbackTry = Try(unsafeMethod()).orElse(Try(backupMethod()))
+  println(fallbackTry)
+
+  // IF you design the API
+  def betterUnsafeMethod(): Try[String] = Failure(new RuntimeException)
+  def betterBackupMethod(): Try[String] = Success("A valid result")
+
+  val betterFallback = betterUnsafeMethod() orElse betterBackupMethod()
+
+  // map, flatMap, filter
+  println(aSuccess.map(_ * 2))
+  println(aSuccess.flatMap(x => Success(x * 10)))
+  println(aSuccess.filter(_ > 10))
+  // => for-comprehensions
+
+  val hostname = "localhost"
+  val port = "8080"
+  def renderHTML(page: String) = print(page)
+
+  class Connection {
+    def get(url: String): String = {
+      val random = new Random(System.nanoTime())
+      if (random.nextBoolean()) "<html>...</html>"
+      else throw new RuntimeException("Connection interrupted")
+    }
+  }
+
+  object HttpService {
+    val random = new Random(System.nanoTime())
+
+    def getConnection(host: String, port: String): Connection = {
+      if (random.nextBoolean()) new Connection
+      else throw new RuntimeException("Someone else took the port")
+    }
+  }
+
+  import HttpService._
+
+  val url = "blog/scala"
+
+  val result = Try(getConnection(hostname, port)).flatMap(
+    connection =>
+      Try(connection.get(url))
+  )
+
+  println(result.getOrElse("no page in second result"))
+
+  // using for comprehensions
+
+ val result2 = for {
+   connection <- Try(getConnection(hostname, port))
+   page <- Try(connection.get(url))
+ } yield page
+
+  println(result2.getOrElse("no page"))
+
+  // if you get the html page from the connection, print it to the console
+}
