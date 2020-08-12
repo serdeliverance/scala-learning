f12867c9875c7b1b17a822814075385f85098217
Sergio Cano <serdeliverance@gmail.com>
2020-08-12 20:42:42 -0300

    advanced inheritance - part 1 - convenience and diamond problem

---
+++ src/main/scala/advance/lectures/part6_ts/RockingInheritance.scala
@@ -0,0 +1,44 @@
+package advance.lectures.part6_ts
+
+object RockingInheritance extends App {
+
+  // convenience
+  trait Writer[T] {
+    def write(value: T): Unit
+  }
+
+  trait Closeable {
+    def close(status: Int): Unit
+  }
+
+  trait GenericStream[T] {
+    // some methods
+    def foreach(f: T => Unit): Unit
+  }
+
+  def processStream[T](stream: GenericStream[T] with Writer[T] with Closeable): Unit = {
+    stream.foreach(println)
+    stream.close(0)
+  }
+
+  // diamond problem
+
+  trait Animal {
+    def name: String
+  }
+
+  trait Lion extends Animal {
+    override def name: String = "lion"
+  }
+
+  trait Tiger extends Animal {
+    override def name: String = "tiger"
+  }
+
+  class Mutant extends Lion with Tiger
+
+  val m = new Mutant
+  println(m.name)       // LAST OVERRIDE GETS PICKED
+
+  // Last override gets picked... thats how scala compiler solves diamond problem
+}
